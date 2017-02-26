bf3c70ba66621592765000449283590effc3d4df
Stuart Bildfell <bildfells760@gmail.com>
2017-02-25 19:13:02 -0700

    Added Don't Starve Together emoticon prepend and other stuff

--- Discord_tail.py
+++ Discord_tail.py
@@ -10,13 +10,51 @@ import argparse
 import os
 import discord
 import asyncio
+import re
 
 client = discord.Client()
+
+async def prepend_dont_starve_emoticon(line):
+    # Join
+    if re.match(r'\[(\d+):(\d+):(\d+)\]: \[Join Announcement\] (.+)$', line):
+        return ":tada: :tada:" + line
+    
+    # Leave
+    elif re.match(r'\[(\d+):(\d+):(\d+)\]: \[Leave Announcement\] (.+)$', line):
+        return ":warning: :warning:" + line
+    
+    # Say
+    elif re.match(r'\[(\d+):(\d+):(\d+)\]: \[Say\] \(.+\) (.+): (.+)$', line):
+        return ":speech_balloon: :speech_balloon:" + line
+    
+    # Whisper
+    elif re.match(r'\[(\d+):(\d+):(\d+)\]: \[Whisper\] \(.+\) (.+): (.+)$', line):
+        return ":envelope: :envelope:" + line
+    
+    # Death
+    elif re.match(r'\[(\d+):(\d+):(\d+)\]: \[Death Announcement\] (.+) was killed by (.+)\.', line):
+        return ":dead: :dead:" + line
+    
+    # Resurrect
+    elif re.match(r'\[(\d+):(\d+):(\d+)\]: \[Resurrect Announcement\] (.+) was resurrected by (.+)\.$', line):
+        return ":heart: :heart:" + line
+    
+    # Skin
+    elif re.match(r'\[(\d+):(\d+):(\d+)\]: \[Skin Announcement\] (.+)$', line):
+        return ":gift: :gift:" + line
+    
+    # Roll
+    elif re.match(r'\[(\d+):(\d+):(\d+)\]: \[Roll Announcement\] \(.+\) (.+) (.+)$', line):
+        return ":game_die: :game_die:" + line
+    
+    # No match found
+    else:
+        return "" + line
 
 async def my_background_task(channelID, filename, time):
     await client.wait_until_ready()
     channel = discord.Object(id=channelID)
-    file = open(filename, 'r')
+    file = open(filename, 'r', encoding='utf-8')
     file.seek(0, os.SEEK_END)
     print('------')
     print('Tailing {} every {} seconds.'.format(filename, time))
@@ -24,9 +62,9 @@ async def my_background_task(channelID, filename, time):
     while not client.is_closed:
         lines = file.readlines()
         file.seek(0, os.SEEK_END)     # Reset EOF flag by seeking to current position
-        if lines != []:    # Not EOF
-            output = ''.join(lines)
-            await client.send_message(channel, output)
+        for line in lines:    # Not EOF
+            line = prepend_dont_starve_emoticon(line)
+            await client.send_message(channel, line)
         await asyncio.sleep(time)
 
 @client.event
@@ -40,13 +78,11 @@ parser = argparse.ArgumentParser(description='Tail a file and output as a Discor
 
 parser.add_argument('--token',
                     '-t',
-                    help='The bot token that will connect to Discord.',
-                    required=True)
+                    help='The bot token that will connect to Discord.')
 parser.add_argument('--channel',
                     '-c',
                     type=int,
-                    help='Discord channel to output to.',
-                    required=True)
+                    help='Discord channel to output to.')
 parser.add_argument('--file',
                     '-f',
                     help='The file to tail.',
@@ -56,7 +92,7 @@ parser.add_argument('--wait',
                     metavar='SEC',
                     type=int,
                     help='Try to read new lines every SEC seconds. (default: 60)',
-                    default=60)
+                    default=30)
 
 args = parser.parse_args()
 
