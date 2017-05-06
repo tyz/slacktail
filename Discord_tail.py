96ba72e49b4aecb08cef67139c0608aadbccb13e
Stuart Bildfell <bildfells760@gmail.com>
2017-05-05 19:33:35 -0600

    Added exception handling

--- Discord_tail.py
+++ Discord_tail.py
@@ -11,13 +11,14 @@ import os
 import discord
 import asyncio
 import re
+import sys
 
 client = discord.Client()
 
 def prepend_dont_starve_emoticon(line):
     # Join
     if re.match(r'\[(\d+):(\d+):(\d+)\]: \[Join Announcement\] (.+)$', line):
-        return ":tada: :tada: " + line
+        return ":balloons: :balloons: " + line
     
     # Leave
     elif re.match(r'\[(\d+):(\d+):(\d+)\]: \[Leave Announcement\] (.+)$', line):
@@ -41,11 +42,19 @@ def prepend_dont_starve_emoticon(line):
     
     # Skin
     elif re.match(r'\[(\d+):(\d+):(\d+)\]: \[Skin Announcement\] (.+)$', line):
-        return ":gift: :gift: " + line
+        return ":gift-1: :gift-1: " + line
     
     # Roll
     elif re.match(r'\[(\d+):(\d+):(\d+)\]: \[Roll Announcement\] \(.+\) (.+) (.+)$', line):
         return ":game_die: :game_die: " + line
+    
+    # Vote
+    elif re.match(r'\[(\d+):(\d+):(\d+)\]: \[Vote Announcement\] (.+)$', line):
+        return "" + line
+    
+    # Kick
+    elif re.match(r'\[(\d+):(\d+):(\d+)\]: \[Kick Announcement\] (.+)$', line):
+        return "" + line
     
     # Announcement
     elif re.match(r'\[(\d+):(\d+):(\d+)\]: \[Announcement\] (.+)$', line):
@@ -58,47 +67,65 @@ def prepend_dont_starve_emoticon(line):
 async def my_background_task(channelID, filename, time):
     await client.wait_until_ready()
     channel = discord.Object(id=channelID)
-    file = open(filename, 'r', encoding='utf-8')
+    
+    try:
+        file = open(filename, 'r', encoding='utf-8')
+    except IOError:
+        sys.exit("There was a problem opening \"{}\", aborting.".format(filename))
+    
     file.seek(0, os.SEEK_END)
-    print('------')
-    print('Tailing {} every {} seconds.'.format(filename, time))
+    print("------")
+    print("Tailing {} every {} seconds.".format(filename, time))
     
     while not client.is_closed:
-        lines = file.readlines()
-        file.seek(0, os.SEEK_END)     # Reset EOF flag by seeking to current position
-        for line in lines:    # Not EOF
-            line = prepend_dont_starve_emoticon(line)
-            await client.send_message(channel, line)
+        try:
+            lines = file.readlines()
+        except UnicodeDecodeError:
+            print("Encountered unknown character in server log, skipping lines.")
+        else:
+            for line in lines:    # Not EOF
+                line = prepend_dont_starve_emoticon(line)    # This line can be removed if you want `line` as is
+                try:
+                    await client.send_message(channel, line)
+                except DiscordException as e:    # For full list of exceptions https://github.com/Rapptz/discord.py/blob/async/discord/errors.py
+                    print("There was a problem communicating with Discord.")
+                    print("TYPE: {}".format(type(e)))
+                    print("ARGS: {}".format(e))
+        
+        file.seek(0, os.SEEK_END)    # Reset EOF flag by seeking to current position
         await asyncio.sleep(time)
 
 @client.event
 async def on_ready():
-    print('Logged in as')
+    print("Logged in as")
     print(client.user.name)
     print(client.user.id)
-    print('------')
+    print("------")
 
-parser = argparse.ArgumentParser(description='Tail a file and output as a Discord bot to a Discord channel.')
+parser = argparse.ArgumentParser(description="Tail a file and output as a Discord bot to a Discord channel.")
 
 parser.add_argument('--token',
                     '-t',
-                    help='The bot token that will connect to Discord.')
+                    help="The bot token that will connect to Discord.")
 parser.add_argument('--channel',
                     '-c',
                     type=int,
-                    help='Discord channel to output to.')
+                    help="Discord channel to output to.")
 parser.add_argument('--file',
                     '-f',
-                    help='The file to tail.',
+                    help="The file to tail.",
                     required=True)
 parser.add_argument('--wait',
                     '-W',
                     metavar='SEC',
                     type=int,
-                    help='Try to read new lines every SEC seconds. (default: 60)',
+                    help="Try to read new lines every SEC seconds. (default: 60)",
                     default=30)
 
 args = parser.parse_args()
 
 client.loop.create_task(my_background_task(args.channel, args.file, args.wait))
-client.run(args.token)
+try:
+    client.run(args.token)
+except LoginFailure:
+    sys.exit("Couldn't login with token \"{}\", aborting.".format(args.token))
